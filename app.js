3d5c2a3dd4094d8b7e1c2a705dee94fa72dc7bdf
Rononoa13 <sumitluitel42@gmail.com>
2020-04-02 18:49:44 +0545

    Make random text appear every 5 sec

--- app.js
+++ app.js
@@ -8,44 +8,52 @@ let animationText = [
     'I Sleep'
 ];
 
-let item = animationText[Math.floor(Math.random() * animationText.length)]
+function animateText() {
 
-console.log(animationText);
+    let item = animationText[Math.floor(Math.random() * animationText.length)]
 
-for (let i = 0; i < animationText.length; i++) {
-    document.getElementById('text-id').innerHTML = item;
+    //console.log(animationText);
 
-}
-
-
-// holds complete element!
-const text = document.querySelector('#text-id');
-// changes complete element to only text content!
-const strText = text.textContent;
-const splitText = strText.split('');
-
-text.textContent = '';
-for (let i = 0; i < splitText.length; i++) {
-    text.innerHTML += '<span>' + splitText[i] + '</span>';
-}
-
-let char = 0;
-
-let timer = setInterval(onTick, 50);
-
-function onTick() {
-    var span = text.querySelectorAll('span')[char];
-    //span.classList.add('fade');
-    span.className += 'fade';
-    char++;
-    if (char == splitText.length) {
-        myStopFunction();
+    for (let i = 0; i < animationText.length; i++) {
+        document.getElementById('text-id').innerHTML = item;
 
     }
 
+    // holds complete element!
+    const text = document.querySelector('#text-id');
+    // changes complete element to only text content!
+    const strText = text.textContent;
+    const splitText = strText.split('');
+
+    text.textContent = '';
+    for (let i = 0; i < splitText.length; i++) {
+        text.innerHTML += '<span>' + splitText[i] + '</span>';
+    }
+
+    let char = 0;
+
+    let timer = setInterval(onTick, 50);
+
+    function onTick() {
+        var span = text.querySelectorAll('span')[char];
+        //span.classList.add('fade');
+        span.className += 'fade';
+        char++;
+        if (char == splitText.length) {
+            myStopFunction();
+
+        }
+
+    }
+
+    function myStopFunction() {
+        clearInterval(timer);
+        timer = '';
+    }
 }
 
-function myStopFunction() {
-    clearInterval(timer);
-    timer = '';
+function main() {
+    setInterval(() => animateText(), 5000);
 }
+
+main();
