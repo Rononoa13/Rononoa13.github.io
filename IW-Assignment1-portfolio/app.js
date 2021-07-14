25d7df3eee81116c611ec4717083a7875634a24c
Rononoa13 <sumitluitel42@gmail.com>
2021-07-14 22:24:56 +0545

    added event listener that shows github repo on click of anchor tag

--- IW-Assignment1-portfolio/app.js
+++ IW-Assignment1-portfolio/app.js
@@ -1,7 +1,17 @@
-fetch('https://api.github.com/users/rononoa13')
-    .then(response => response.json())
-    .then(function(data) {
-        console.log(data)
+fetch("https://api.github.com/users/rononoa13")
+  .then((response) => response.json())
+  .then(function (data) {
+    console.log(data);
 
-        
+    document.getElementById("profileImage").src = data["avatar_url"];
+
+// shows paragraph after click of anchor tag`with id myProject`
+    let project = document.getElementById('showProject')
+    project.addEventListener("click", function() {
+        document.getElementById('myProject').textContent = `I have ` + data["public_repos"] + ` repos`
     })
+    console.log(project)
+    // return data;
+  });
+
+
