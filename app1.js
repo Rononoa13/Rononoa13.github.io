1abcb9689d22c06e682985c78c67cb7dac4ea43c
Sumit Luitel <sltel@Sumits-MacBook-Pro.local>
2025-10-18 11:29:33 +0545

    Fix animation interval and text

--- app1.js
+++ app1.js
@@ -1,29 +1,40 @@
 let animationText = [
-    "I love Python 👨‍💻",
-    "I play BasketBall 🏀 ⛹️‍♂️",
-    "I like photography 📷",
-    "I drum my heart out 🎶 🥁",
-    "I love to Code 🤖",
-    "I read books 📚",
-    "I am from Nepal 🇳🇵",
-    "I am a Software Engineer 🧑🏽‍💻",
-    "I love Flask 👨‍💻",
-    "I love to swim 🏊‍♂️",
-    "I love to travel 🧳 🗺️",
-    "I love chess ♟️",
-    "I love music 🎧", 
-    "I play X-Box 🎮",
-    "I love to cook 🍳",
-    "I love linux 💻"
+    "I code in Python like a wizard 👨‍💻✨",
+    "I dribble a basketball like a champ 🏀⛹️‍♂️",
+    "I capture moments one click at a time 📷",
+    "I drum my heart out and make beats 🎶🥁",
+    "I tinker with code and bring machine to life 🤖",
+    "I get lost in books and find new worlds 📚",
+    "Nepal is home, and proud I am 🇳🇵",
+    "I build software and break things elegantly 🧑🏽‍💻",
+    "Flask and FastAPI is my secret weapon for web magic 👨‍💻",
+    "I dive into the water and conquer waves 🏊‍♂️",
+    "I wander the world with my backpack 🧳🗺️",
+    "Chess is my battlefield, and pawns fear me ♟️",
+    "I vibe with music and let it move me 🎧",
+    "I conquer virtual worlds on X-Box 🎮 and playstation ",
+    "I cook like a mad scientist in the kitchen 🍳",
+    "I hack around on Linux and feel unstoppable 💻"
 ];
 
 function selectFromList() {
   return animationText[Math.floor(Math.random() * animationText.length)];
 }
 
+let firstTime = true;
 function animateText() {
   const textElement = document.getElementById("text-id");
-  const item = selectFromList();
+  let item;
+
+  if (firstTime) {
+    item = "Hello, World 🌎";
+    firstTime = false;
+  } else {
+    // pick random from animationText excluding Hello World
+    item = animationText[Math.floor(Math.random() * (animationText.length - 1)) + 1];
+  }
+
+//   const item = selectFromList();
 
   textElement.textContent = ""; // clear previous text
   const splitText = Array.from(item); // handles emojis correctly
@@ -38,7 +49,7 @@ function animateText() {
 
   let char = 0;
   const spans = textElement.getElementsByTagName("span");
-  const interval = 150; // time per letter in ms
+  const interval = 90; // time per letter in ms
 
   const timer = setInterval(() => {
     if (char < spans.length) {
@@ -47,7 +58,7 @@ function animateText() {
     } else {
       clearInterval(timer);
       // After the last letter fades in, wait 2s then start next animation
-      setTimeout(animateText, 2000);
+      setTimeout(animateText, 1000);
     }
   }, interval);
 }
